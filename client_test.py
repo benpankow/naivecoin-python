e2d2d348ae6f0d4dee9450094d122f089c112441
Ben Pankow <pankow.ben@gmail.com>
2018-02-25 15:33:23 -0600

    Continue tinkering with socket networking

--- client_test.py
+++ client_test.py
@@ -1,8 +1,19 @@
 import socket, pickle
 from block import *
+from enum import Enum
+
+class MessageType(Enum):
+    QUERY_LATEST_BLOCK = 0
+    QUERY_ALL = 1
+    RESPONSE_BLOCKCHAIN = 2
+
+class Message():
+    def __init__(self, message_type, message_data):
+        self.type = message_type
+        self.data = message_data
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect(('', 5000))
 create_next_block('test')
-dumpo = b'2 ' + pickle.dumps(get_blockchain())
-client_socket.send(dumpo)
+dump = pickle.dumps(Message(MessageType.RESPONSE_BLOCKCHAIN, get_blockchain()))
+client_socket.send(dump)
--- main.py
+++ main.py
@@ -1,19 +1,35 @@
 import socket, select, pickle
 from block import *
+from enum import Enum
+
+class MessageType(Enum):
+    QUERY_LATEST_BLOCK = 0
+    QUERY_ALL = 1
+    RESPONSE_BLOCKCHAIN = 2
+
+class Message():
+    def __init__(self, message_type, message_data, reply_port):
+        self.type = message_type
+        self.data = message_data
+        self.reply_port = reply_port
+
+socket_num = int(input('Base socket: '))
 
 control_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 control_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-control_socket.bind(('', 8000))
+control_socket.bind(('', socket_num))
 control_socket.listen(1)
 
 p2p_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 p2p_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-p2p_socket.bind(('', 5000))
+p2p_socket.bind(('', socket_num + 1))
 p2p_socket.listen(1)
 
 poll = select.poll()
 poll.register(control_socket.fileno())
 poll.register(p2p_socket.fileno())
+
+peer_sockets = []
 
 while True:
     fd = poll.poll()
@@ -29,26 +45,43 @@ while True:
                 conn_socket.send(b'HTTP/1.0 200 OK\r\n\r\n')
                 for block in get_blockchain():
                     conn_socket.send(str(block).encode())
-            if endpoint == b'/mine':
+            elif endpoint == b'/mine':
                 conn_socket.send(b'HTTP/1.0 200 OK\r\n\r\n')
                 new_block = create_next_block('test')
                 conn_socket.send(str(new_block).encode())
+            elif b'/add-peer/' in endpoint:
+                peer_socket_num = int(endpoint[len(b'/add-peer/'):].decode())
+
+                peer_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                peer_socket.connect(('', peer_socket_num))
+                peer_sockets.append(peer_socket)
+                peer_socket.send(pickle.dumps(Message(MessageType.QUERY_ALL, '', socket_num + 1)))
+                print(peer_socket)
+
+                conn_socket.send('Connected to peer {}'.format(socket_num).encode())
             conn_socket.close()
         except IOError:
             conn_socket.close()
     else:
         conn_socket, addr = p2p_socket.accept()
+
         message = conn_socket.recv(4096)
-        if len(message) > 2:
-            message_type = int(chr(message[0]))
-            message = message[2:]
-            print(message_type)
-            if message_type == 2:
-                block_list = pickle.loads(message)
-                if (replace_blockchain(block_list)):
-                    print('Got longer chain!')
-                else:
-                    print('Got worse chain')
+        message = pickle.loads(message)
+        if message.type == MessageType.RESPONSE_BLOCKCHAIN:
+            if (replace_blockchain(message.data)):
+                print('Got longer chain!')
+            else:
+                print('Got worse chain')
+        elif message.type == MessageType.QUERY_ALL:
+            print('doot doot')
+            print(addr)
+            print(conn_socket)
+            peer_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            peer_socket.connect((addr[0], message.reply_port))
+            peer_socket.send(pickle.dumps(Message(MessageType.QUERY_LATEST_BLOCK, '', socket_num + 1)))
+
+        elif message.type == MessageType.QUERY_LATEST_BLOCK:
+            print('deet')
     '''conn_socket, addr = control_socket.accept()
     try:
         message = conn_socket.recv(1024)
